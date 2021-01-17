fe60ddb1d92e81f58aa242333ae15b85482d5a45
mc2918 <tlsdud1375@gmail.com>
2021-01-17 23:07:11 +0900

    Fix html and Add button style, html

--- src/App.js
+++ src/App.js
@@ -4,24 +4,43 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  let [ title, setTitle ] = useState({ 0 : 'title1', 1: 'title2', 2: 'title3'});
+  let [ title, setTitle ] = useState({ 0 : '남자코드 추천', 1: '강남 맛집', 2: '여의도 맛집'});
+  let [ count, upDateCount] = useState(0);
 
   return (
     <div className="App">
       <div className="black-nav">
         <h1>개발자 Blog</h1>
       </div>
+      <div className="button__wrap">
+        <button type="button" className="button button--modify">수정</button>
+      </div>
       <ul className="list">
         <li className="list__item">
-          <h3>{title[0]}</h3>
+          <div className="list__title">
+            <h3>{title[0]}</h3>
+            <div className="like-count">
+              <span onClick={ () => { upDateCount(count + 1) } }>👍🏻</span>{count}
+            </div>
+          </div>
           <p>발행일 2월 17일</p>
         </li>
         <li className="list__item">
-          <h3>{title[1]}</h3>
+          <div className="list__title">
+            <h3>{title[1]}</h3>
+            <div className="like-count">
+              <span onClick={ () => { upDateCount(count + 1) } }>👍🏻</span>{count}
+            </div>
+          </div>
           <p>발행일 2월 17일</p>
         </li>
         <li className="list__item">
-          <h3>{title[2]}</h3>
+          <div className="list__title">
+            <h3>{title[2]}</h3>
+            <div className="like-count">
+              <span onClick={ () => { upDateCount(count + 1) } }>👍🏻</span>{count}
+            </div>
+          </div>
           <p>발행일 2월 17일</p>
         </li>
       </ul>
