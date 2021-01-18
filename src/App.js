6f6d50343feba8e973934cbd9fb2d764c9c74727
mc2918 <tlsdud1375@gmail.com>
2021-01-18 16:22:55 +0900

    Add sort function

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
 /* eslint-disable */
 import React, { useState } from 'react';
+import { faFilter } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './App.css';
 
 // 정렬기능 정렬 추가 에정
 
 function App() {
-  let [ title, setTitle ] = useState({ 0 : '남자코드 추천', 1: '강남 맛집', 2: '여의도 맛집'});
+  let [ title, setTitle ] = useState(['남자코드', '하남 맛집', '여의도 맛집']);
   let [ count, setCount] = useState(0);
 
   function handleButtonClick() {
-    const newData = { ...title } // deep copy array랑 objdect랑 생각
+    const newData = [...title]; // deep copy array랑 objdect랑 생각
     newData[0] = '여자코트 추천';
     setTitle(newData);
   }
+
+  function compare ( a, b) {
+    if(a < b) {
+      return -1;
+    } 
+    if( a > b) {
+      return 1;
+    }
+    return 0
+  }
+
+   function sortButtonClick() {
+    const sortData = [...title];
+    sortData.sort(compare);
+    setTitle(sortData)
+   }
 
   return (
     <div className="App">
@@ -20,6 +38,7 @@ function App() {
         <h1>개발자 Blog</h1>
       </div>
       <div className="button__wrap">
+        <button type="button" className="button button--sort" onClick={ sortButtonClick }><FontAwesomeIcon icon={ faFilter } />정렬</button>
         <button type="button" className="button button--modify" onClick={ handleButtonClick }>수정</button>
       </div>
       <ul className="list">
